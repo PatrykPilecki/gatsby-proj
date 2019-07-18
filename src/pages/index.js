8db756f7f5da40238cacb98e5e7a24129b57977d
PatrykP <patryk.pilecki@onet.eu>
2019-07-18 20:44:34 +0200

    Add grid-layout and new psot

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,12 +19,21 @@ const Template2 = (props) => (
    
   </div>
 )
+const Template3 = (props) => ( 
+  <div className={`post-list ${props.templateName}`}>
+    <h1>{props.node.frontmatter.title}</h1>
+    <span>{props.node.frontmatter.date}</span>
+   
+  </div>
+)
 
 const PostTemplate = (props) => {
   const selectedTemplate = (
     props.index === 0 && <Template1 templateName={props.templateName} node={props.node}/>
     ) || (
       props.index >= 1 && <Template2 templateName={props.templateName} node={props.node}/>
+      ) || (
+        props.index >= 2 && props.index < 6 && <Template3 templateName={props.templateName} node={props.node}/>
       )
   return selectedTemplate;
 }
@@ -40,12 +49,12 @@ const IndexPage = (props) => {
         const templateName = (
           i === 0 && 'template-1') ||
           (i === 1 && 'template-2') ||
-          (i >-2 && i < 5  && 'template-3');
+          (i >= 2 && i < 6  && 'template-3');
 
         return (
           <Link to={node.fields.slug} key={i} id="link-style" className={templateName} >
          
-              <PostTemplate index={i} templateName={templateName} node={node} />
+              <PostTemplate index={i}  node={node} />
               
           </Link>
         )
